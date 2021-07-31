6b90f3ead0fbc6ecc5a669ffa45f2927fd5eb298
georgeh <georgeh@sfu.ca>
2021-07-31 16:10:12 -0700

    save changes

--- fptree.cpp
+++ fptree.cpp
@@ -419,7 +419,7 @@ inline LeafNode* FPtree::findLeafAndPushInnerNodes(uint64_t key)
     return reinterpret_cast<LeafNode*> (cursor);
 }
 
-InnerNode* findParent(uint64_t key, BaseNode* child)
+InnerNode* FPtree::findParent(uint64_t key, BaseNode* child)
 {
     if (!root)
         return nullptr;
