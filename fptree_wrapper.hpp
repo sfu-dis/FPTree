98c25fa65070fe188ec4ae163e8b440c00cceaaf
georgeh <georgeh@sfu.ca>
2021-09-26 20:28:29 -0700

    wrapper update

--- fptree_wrapper.hpp
+++ fptree_wrapper.hpp
@@ -9,6 +9,7 @@
 #include <shared_mutex>
 #include <libpmemobj.h>
 
+// #define DEBUG_MSG
 
 class fptree_wrapper : public tree_api
 {
@@ -40,7 +41,12 @@ bool fptree_wrapper::find(const char* key, size_t key_sz, char* value_out)
     // For now only support 8 bytes key and value (uint64_t)
     uint64_t value = tree_.find(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)));
     if (value == 0)
+    {
+#ifdef DEBUG_MSG
+        printf("Search key not found!\n");
+#endif
         return false;
+    }
     memcpy(value_out, &value, sizeof(value));
     return true;
 }
@@ -49,20 +55,38 @@ bool fptree_wrapper::find(const char* key, size_t key_sz, char* value_out)
 bool fptree_wrapper::insert(const char* key, size_t key_sz, const char* value, size_t value_sz)
 {
     KV kv = KV(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)), *reinterpret_cast<uint64_t*>(const_cast<char*>(value)));
-    tree_.insert(kv);
+    if (!tree_.insert(kv))
+    {
+#ifdef DEBUG_MSG
+        printf("Insert failed\n");
+#endif
+        return false;
+    }
     return true;
 }
 
 bool fptree_wrapper::update(const char* key, size_t key_sz, const char* value, size_t value_sz)
 {
     KV kv = KV(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)), *reinterpret_cast<uint64_t*>(const_cast<char*>(value)));
-    tree_.update(kv);
+    if (!tree_.update(kv))
+    {
+#ifdef DEBUG_MSG
+        printf("Update failed\n");
+#endif
+        return false;
+    }
     return true;
 }
 
 bool fptree_wrapper::remove(const char* key, size_t key_sz)
 {
-    tree_.deleteKey(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)));
+    if (!tree_.deleteKey(*reinterpret_cast<uint64_t*>(const_cast<char*>(key))))
+    {
+#ifdef DEBUG_MSG
+        printf("Remove failed\n");
+#endif
+        return false;
+    }
     return true;
 }
 
@@ -70,7 +94,11 @@ int fptree_wrapper::scan(const char* key, size_t key_sz, int scan_sz, char*& val
 {
     constexpr size_t ONE_MB = 1ULL << 20;
     static thread_local char results[ONE_MB];
-    return tree_.rangeScan(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)), (uint64_t)scan_sz, results);
+    int scanned = tree_.rangeScan(*reinterpret_cast<uint64_t*>(const_cast<char*>(key)), (uint64_t)scan_sz, results);
+#ifdef DEBUG_MSG
+    printf("%d records scanned\n", scanned);
+#endif
+    return scanned;
 }
 
 #endif
